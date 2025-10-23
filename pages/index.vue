cddee6e39cd94637b745e3a76fe877b1ffd6d5c3
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2025-10-23 12:33:54 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="text-3xl font-bold">Welcome to Nuxt 3</h1>
+  <p class="mt-4">This is your starter template.</p>
+  <UCard>
+    <template #header>
+      <Placeholder class="h-8" />
+    </template>
+
+    <Placeholder class="h-32" />
+
+    <template #footer>
+      <Placeholder class="h-8" />
+    </template>
+    <UButton color="neutral">Button</UButton>
+  </UCard>
+</template>
